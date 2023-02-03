3822251534efa894259f861599cdf9c0f100c4e9
ad3pt7 <denisstorozhev1@yandex.ru>
2023-02-03 19:19:08 +0500

    First commit

---
+++ app/src/main/java/com/example/giftshop/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.giftshop;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+        User user = new User("admifdsfn1","admin121");
+        mDatabase.child("admin1").setValue(user);
+        mDatabase.child("aboba").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+    }
+
+    public void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        mDatabase.child("users").child(userId).setValue(user);
+    }
+}
---
+++ app/src/main/java/com/example/giftshop/User.java
@@ -0,0 +1,17 @@
+package com.example.giftshop;
+
+public class User {
+
+    public String login;
+    public String password;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.login = username;
+        this.password = email;
+    }
+
+}
