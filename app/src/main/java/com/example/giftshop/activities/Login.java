b682bbbf759fa38a54247a2600a13817917c926c
Alexander <lenchenkov.a.d@gmail.com>
2023-02-04 19:22:16 +0500

    login page, preference manager, constants, login activity

--- app/src/main/java/com/example/giftshop/activities/Login.java
+++ app/src/main/java/com/example/giftshop/activities/Login.java
@@ -2,20 +2,93 @@ package com.example.giftshop.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.giftshop.databinding.ActivityLoginBinding;
+import com.example.giftshop.utilities.Constants;
+import com.example.giftshop.utilities.PreferenceManager;
 
 public class Login extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        if (preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 
+    private void setListeners() {
+        binding.buttonLogin.setOnClickListener(v -> {
+            if (isValidSignInDetails()) {
+                signIn();
+            }
+        });
+    }
 
+    private void signIn() {
+        loading(true);
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        database.collection(Constants.KEY_COLLECTION_USERS)
+//                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+//                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+//                .get()
+//                .addOnCompleteListener(task -> {
+//                    if (task.isSuccessful() && task.getResult() != null
+//                            && task.getResult().getDocuments().size() > 0) {
+//                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+//                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+//                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+//                        preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+//                        preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+//                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                        startActivity(intent);
+//                    } else {
+//                        loading(false);
+//                        showToast("Unable to sign in");
+//                    }
+//                });
+    }
+
+    public void loading(boolean isLoading) {
+        if (isLoading) {
+            binding.buttonLogin.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonLogin.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidSignInDetails() {
+        if (binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("Enter email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+            showToast("Enter valid email");
+            return false;
+        } else if (binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast("Enter password");
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
