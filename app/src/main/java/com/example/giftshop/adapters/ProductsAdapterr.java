ddae395ba7ab45e1fcb99f96c6dbc530e9595e02
Alexander <lenchenkov.a.d@gmail.com>
2023-02-04 19:40:57 +0500

    add like

---
+++ app/src/main/java/com/example/giftshop/adapters/ProductsAdapterr.java
@@ -0,0 +1,19 @@
+package com.example.giftshop.adapters;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.giftshop.models.Product;
+
+import java.util.List;
+
+public class ProductsAdapterr {
+
+    private final List<Product> products;
+    private final ProductListener productListener;
+
+    class ProductVIewHolder extends RecyclerView.ViewHolder {
+        
+    }
+
+
+}
